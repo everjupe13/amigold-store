a608bc073efaaec50b116acf76a848994b99527e
Ilya Bakirov <everjupe13@gmail.com>
2023-09-09 22:12:52 +0300

    fix: change user store, add error catcher

--- store/user/useUserStore.ts
+++ store/user/useUserStore.ts
@@ -1,34 +1,12 @@
 import { defineStore } from 'pinia'
 
 import { useFetchApiRequest } from '@/composables/useApiRequest'
-import { useAuthStore } from '@/store/user/auth/useAuthStore'
+import { HttpCatchedError } from '@/utils/network/error.service'
+
+import { useAuthStore } from './auth/useAuthStore'
+import { type IUserData } from './user.types'
 
 export const TOKEN_LOCAL_STORAGE_KEY = 'x-token'
-
-type IUserData = {
-  id: number
-  client: number
-  pages: {
-    id: number
-    page: {
-      id: number
-      name: string
-      url: string
-    }
-    permission: {
-      id: 0
-      name: string
-      can_open: boolean
-      can_edit: boolean
-    }
-    role: number
-  }[]
-  name: string
-  email: string
-  phone: string
-  comment: string
-  is_manager: boolean
-}
 
 export const useUserStore = defineStore('user', () => {
   const authStore = useAuthStore()
@@ -36,40 +14,6 @@ export const useUserStore = defineStore('user', () => {
   const isAuth = computed<boolean>(() =>
     Boolean(user.value && Object.keys(user.value).every(field => !!field))
   )
-
-  const createUser = async ({
-    login,
-    password
-  }: {
-    login: string
-    password: string
-  }) => {
-    const { status } = await authStore.signUp({ login, password })
-    if (status) {
-      const authUserApiResponse = await authUser({ login, password })
-
-      return { status: authUserApiResponse.status }
-    }
-
-    return { status: false }
-  }
-
-  const authUser = async ({
-    login,
-    password
-  }: {
-    login: string
-    password: string
-  }) => {
-    const { status } = await authStore.signIn({ login, password })
-    if (status) {
-      await _fetchUserData()
-
-      return { status: true }
-    }
-
-    return { status: false }
-  }
 
   const _fetchUserData = async () => {
     try {
@@ -87,8 +31,42 @@ export const useUserStore = defineStore('user', () => {
         return { data: response, error: null, status: true }
       }
     } catch (error) {
-      return { status: false, error }
+      return HttpCatchedError({ error })
     }
+  }
+
+  const createUser = async ({
+    login,
+    password
+  }: {
+    login: string
+    password: string
+  }) => {
+    const { status } = await authStore.signUp({ login, password })
+    if (status) {
+      const authUserApiResponse = await authUser({ login, password })
+
+      return { status: authUserApiResponse.status }
+    }
+
+    return HttpCatchedError()
+  }
+
+  const authUser = async ({
+    login,
+    password
+  }: {
+    login: string
+    password: string
+  }) => {
+    const { status } = await authStore.signIn({ login, password })
+    if (status) {
+      await _fetchUserData()
+
+      return { status: true }
+    }
+
+    return HttpCatchedError()
   }
 
   const initializeUser = async () => {
@@ -98,5 +76,14 @@ export const useUserStore = defineStore('user', () => {
     }
   }
 
-  return { isAuth, user, createUser, authUser, initializeUser }
+  const forget = async () => {
+    const { data, error, status } = await authStore.signOut()
+    if (status) {
+      return { data, error, status }
+    }
+
+    return HttpCatchedError({ error })
+  }
+
+  return { isAuth, user, createUser, authUser, initializeUser, forget }
 })
