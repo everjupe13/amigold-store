4fbd3faa4aaa8a57527f1a24bf033e3603397e1a
Ilya Bakirov <everjupe13@gmail.com>
2023-09-09 22:07:43 +0300

    lint: clean comments

--- components/features/profile/ProfileUserForm.vue
+++ components/features/profile/ProfileUserForm.vue
@@ -5,7 +5,6 @@ import { email, required } from '@vuelidate/validators'
 import { useModal } from 'vue-final-modal'
 
 import ProfileUserEditModal from '@/components/features/profile/ProfileUserEditModal.vue'
-import { useCartStore } from '@/store/cart'
 
 const formData = reactive({
   email: 'example@mailbox.io',
@@ -21,12 +20,7 @@ const rules = {
   password: { required }
 }
 
-// const emit = defineEmits<{
-//   (e: 'confirm'): void
-// }>()
-
 const v$ = useVuelidate(rules, formData)
-const cartStore = useCartStore()
 const isLoading = ref(false)
 const isSuccess = ref(false)
 
@@ -39,51 +33,11 @@ const { open, close } = useModal({
   }
 })
 
-const successScreen = ref(false)
-
 onMounted(() => {
-  isLoading.value = true
-  isSuccess.value = false
-  v$.value.$validate()
   isLoading.value = false
   isSuccess.value = true
 })
 
-const onSubmitForm = async () => {
-  return
-  isLoading.value = true
-  isSuccess.value = false
-  if (v$.value.$invalid) {
-    v$.value.$validate()
-    isLoading.value = false
-    return
-  }
-
-  await cartStore.createOrder({
-    email: formData.email,
-    name: formData.name,
-    phone: formData.phone,
-    comment: formData.password
-  })
-
-  isLoading.value = false
-  isSuccess.value = true
-  setTimeout(() => {
-    isSuccess.value = false
-
-    formData.email = ''
-    formData.name = ''
-    formData.phone = ''
-    formData.password = ''
-    v$.value.$reset()
-
-    successScreen.value = true
-  }, 200)
-}
-
-// const closeModal = () => {
-//   emit('confirm')
-// }
 const inputPropsMapper = (props: { [x: string]: any }) => {
   return {
     isValid: props.$dirty ? !props.$invalid : true,
@@ -94,12 +48,12 @@ const inputPropsMapper = (props: { [x: string]: any }) => {
 </script>
 
 <template>
-  <form @submit.prevent="onSubmitForm">
+  <form @submit.prevent="">
     <ProfileFormInput
       v-model="v$.email.$model"
       placeholder="Е-mail"
       class="mb-15"
-      :disabled="isSuccess || isLoading || true"
+      :readonly="true"
       v-bind="inputPropsMapper(v$.email)"
       @click="open"
     />
@@ -107,7 +61,7 @@ const inputPropsMapper = (props: { [x: string]: any }) => {
       v-model="v$.phone.$model"
       placeholder="Номер телефона"
       class="mb-15"
-      :disabled="isSuccess || isLoading || true"
+      :readonly="true"
       v-bind="inputPropsMapper(v$.phone)"
       @click="open"
     />
@@ -115,7 +69,7 @@ const inputPropsMapper = (props: { [x: string]: any }) => {
       v-model="v$.name.$model"
       placeholder="ФИО"
       class="mb-15"
-      :disabled="isSuccess || isLoading || true"
+      :readonly="true"
       v-bind="inputPropsMapper(v$.name)"
       @click="open"
     />
@@ -123,8 +77,7 @@ const inputPropsMapper = (props: { [x: string]: any }) => {
       v-model="v$.password.$model"
       placeholder="Пароль"
       type="password"
-      class="mb-15"
-      :disabled="isSuccess || isLoading || true"
+      :readonly="true"
       v-bind="inputPropsMapper(v$.password)"
       @click="open"
     />
