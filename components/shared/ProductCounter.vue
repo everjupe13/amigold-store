d6732f8d0eed30c24943d19f8dfd4d90737f9111
Ilya Bakirov <everjupe13@gmail.com>
2023-09-13 01:06:33 +0300

    feat: make components for code

---
+++ components/shared/ProductCounter.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+import { MinusIcon, PlusIcon } from '@/components/shared/icons'
+
+const props = withDefaults(
+  defineProps<{
+    amount: number
+    isDisabled?: boolean
+    disabledWhenCount?: number
+    buttonClasses?: string
+    wrapperClasses?: string
+  }>(),
+  {
+    amount: 1,
+    isDisabled: false,
+    disabledWhenCount: 1,
+    buttonClasses: '',
+    wrapperClasses: ''
+  }
+)
+
+const emits = defineEmits<{
+  (e: 'increase'): () => void
+  (e: 'decrease'): () => void
+}>()
+
+const decreaseCount = () => {
+  emits('decrease')
+}
+
+const increaseCount = () => {
+  emits('increase')
+}
+</script>
+
+<template>
+  <div class="flex items-center gap-x-12" :class="wrapperClasses">
+    <button
+      class="group flex h-24 w-24 items-center justify-center rounded-full border-[2px] border-gray transition disabled:cursor-default disabled:opacity-20"
+      :disabled="
+        (props.disabledWhenCount !== undefined &&
+          props.amount === props.disabledWhenCount) ||
+        isDisabled
+      "
+      :class="[
+        props.buttonClasses
+          ? props.buttonClasses
+          : 'bg-green hover:border-green hover:bg-green'
+      ]"
+      @click="decreaseCount"
+    >
+      <MinusIcon class="text-gray transition group-hover:text-white" />
+    </button>
+    <div class="min-w-30 leading-nonde font-inter text-bold-24">
+      {{ props.amount }}
+    </div>
+    <button
+      class="group flex h-24 w-24 items-center justify-center rounded-full border-[2px] border-gray transition disabled:cursor-default disabled:opacity-20"
+      :disabled="isDisabled"
+      :class="[
+        props.buttonClasses
+          ? props.buttonClasses
+          : 'bg-green hover:border-green hover:bg-green'
+      ]"
+      @click="increaseCount"
+    >
+      <PlusIcon class="text-gray transition group-hover:text-white" />
+    </button>
+  </div>
+</template>
