cf43fdd9b93567ed397fb1daa34a6a5f29472d71
Ilya Bakirov <everjupe13@gmail.com>
2023-10-10 14:18:12 +0300

    fix: fix home categories card icon

--- components/screens/app-home/HomeCategoriesCard.vue
+++ components/screens/app-home/HomeCategoriesCard.vue
@@ -15,7 +15,7 @@ const toLink = computed(() => `/catalog${props.slug ? `/${props.slug}` : ''}`)
 
 <template>
   <article
-    class="group relative flex aspect-square flex-col gap-y-20 overflow-hidden rounded-[24px] bg-white p-20 md:aspect-auto md:h-[180px]"
+    class="group relative flex h-[210px] flex-col justify-between overflow-hidden rounded-[24px] bg-white p-20 md:aspect-auto md:h-[180px]"
     :class="{
       'animate-pulse !bg-[var(--skeleton-loader-bg,_rgb(238,239,239))]':
         !props.name || !props.image,
@@ -27,16 +27,16 @@ const toLink = computed(() => `/catalog${props.slug ? `/${props.slug}` : ''}`)
       class="absolute inset-0 z-[2] block transition duration-500"
     ></NuxtLink>
     <div
-      class="relative z-[1] mx-auto flex max-h-[150px] flex-shrink items-center justify-center p-10 md:max-h-[100px]"
+      class="relative z-[1] mx-auto flex max-h-[120px] flex-shrink items-center justify-center p-10 md:max-h-[100px]"
     >
       <img
         v-if="props.image"
         :src="props.image"
         alt=""
-        class="pointer-events-none relative z-[1] block h-full select-none object-contain transition duration-200 group-hover:scale-110"
+        class="pointer-events-none block select-none object-contain transition duration-200 group-hover:scale-110"
       />
     </div>
-    <div class="relative z-[1] mt-auto flex-shrink-0 flex-grow-0">
+    <div class="relative z-[1] flex-shrink-0">
       <h3
         class="text-center leading-tight text-black transition text-bold-18 group-hover:text-white"
       >
